2a5dc0357c285bd7c24342abac4ffa135cfc1f06
Ryan Tsao <rtsao@uber.com>
2016-02-23 02:29:57 -0800

    ensure document exists, update exports

--- csjs.js
+++ csjs.js
@@ -6,9 +6,10 @@ var insertCss = require('insert-css');
 function csjsInserter() {
   var args = Array.prototype.slice.call(arguments);
   var result = csjs.apply(null, args);
-  insertCss(csjs.getCss(result));
+  if (global.document) {
+    insertCss(csjs.getCss(result));
+  }
   return result;
 }
 
 module.exports = csjsInserter;
-module.exports.getCss = csjs.getCss;
